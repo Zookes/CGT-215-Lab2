398d48bd16f12f7b85e84dea379e1c6bea0c5f89
Zookes <90047175+Zookes@users.noreply.github.com>
2024-08-28 14:06:53 -0400

    Update Lab2.cpp
    The actual completed code.

--- Lab2.cpp
+++ Lab2.cpp
@@ -8,13 +8,20 @@ int main()
 	float B;
 	float X;
 	
-	cout << "Hi, my name is Liam and this is simple program to multiply two numbers together.";
+	cout << "Hi, my name is Liam and this is simple program to solve the equation: \n";
+	cout << "Ax + B = 0\n"; 
+	cout << "For x\n\n";
+	
 	cout << "Please enter a value for A: ";
 	cin >> A;
 	cout << "Please enter a value for B: ";
 	cin >> B;
-	
-	X = (A * B) 
-	
-	cout << "Your total is " << X << end1;
+
+    X = (0 - B)/A;
+    
+    cout<<"\nSolving " << A << "x +" << B << "= 0 for x..."; 
+
+    cout<<"\n\nThe answer is: \n";
+    cout<< "x = " << X << endl;
 }
+
