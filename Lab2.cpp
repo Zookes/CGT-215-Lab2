bd4d6f6d31a10018cc58f64d1eb39d30dbce38a2
Zookes <90047175+Zookes@users.noreply.github.com>
2024-08-28 13:55:16 -0400

    Update Lab2.cpp
    Finishing touches.

--- Lab2.cpp
+++ Lab2.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
+
 using namespace std;
+
 int main()
 {
 	float A;
 	float B;
 	float X;
-	cout << "Hi, my name is Liam and this is "
+	
+	cout << "Hi, my name is Liam and this is simple program to multiply two numbers together.";
 	cout << "Please enter a value for A: ";
 	cin >> A;
-	cout << "A=" << A << endl;
-	B = (A * 4) + 1;
-	cout << "And B=" << B << endl;
+	cout << "Please enter a value for B: ";
+	cin >> B;
+	
+	X = (A * B) 
+	
+	cout << "Your total is " << X << end1;
 }
