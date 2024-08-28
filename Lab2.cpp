9f8402f312a2ebe4bdebc56dd1d803d58717b188
farr12 <farr12@X-KNOY416PC14>
2024-08-28 13:45:16 -0400

    Add project files.

---
+++ Lab2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float A;
+	float B;
+	float X;
+	cout << "Hi, my name is Liam and this is "
+	cout << "Please enter a value for A: ";
+	cin >> A;
+	cout << "A=" << A << endl;
+	B = (A * 4) + 1;
+	cout << "And B=" << B << endl;
+}
